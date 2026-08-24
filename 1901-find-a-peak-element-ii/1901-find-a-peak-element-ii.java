785d1e2cbca647561c9cde553a9ef9b93f3bca71
Pritam Nayak <113450331+Pritam943@users.noreply.github.com>
2026-08-24 22:37:50 +0530

    Time: 0 ms (100%), Space: 119.1 MB (78.13%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
@@ -5,45 +5,47 @@ class Solution {
         int m = mat[0].length;
 
         int low = 0;
-        int high = m - 1;
+        int high = m-1;
 
-        while (low <= high) {
+        int[] ans = new int[2];
 
-            int mid = low + (high - low) / 2;
+        while(low <= high){
 
-            int row = foundMaxRow(mat, mid, n);
+            int mid = low + (high - low)/2;
 
-            int left = mid - 1 >= 0 ? mat[row][mid - 1] : -1;
-            int right = mid + 1 < m ? mat[row][mid + 1] : -1;
+            int row = findMaxRow(mat, mid, n);
 
-            if (left < mat[row][mid] && mat[row][mid] > right) {
-                return new int[] { row, mid };
+            int left = (mid - 1 >= 0) ? mat[row][mid-1] : -1;
+            int right = (mid + 1 < m) ? mat[row][mid+1] : -1;
+
+            if(left < mat[row][mid] && mat[row][mid] > right){
+                return new int[]{row, mid};
             }
-
-            else if (left > mat[row][mid])
+            else if(left > mat[row][mid]){
                 high = mid - 1;
-            else
-                low = mid + 1;
+            }
+            else{
+                low = mid+1;
+            }
         }
 
-        return new int[] { -1, -1 };
-
+        return new int[]{-1, -1};
+        
     }
 
-    public int foundMaxRow(int[][] mat, int m, int n) {
+    public int findMaxRow(int[][] mat, int col, int n){
 
-        int maxEle = Integer.MIN_VALUE;
-        int maxI = -1;
+        int max = Integer.MIN_VALUE;
+        int maxRow = -1;
 
-        for (int i = 0; i < n; i++) {
-
-            if (mat[i][m] > maxEle) {
-                maxEle = Math.max(maxEle, mat[i][m]);
-                maxI = i;
+        for(int i=0; i<n; i++){
+            
+            if(mat[i][col] > max){
+                max = Math.max(max, mat[i][col]);
+                maxRow = i;
             }
-
         }
 
-        return maxI;
+        return maxRow;
     }
 }
